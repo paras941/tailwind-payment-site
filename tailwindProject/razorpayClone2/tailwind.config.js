b7ac8d0482ddda7791b7633e522715779620dd88
paras <rawatparas@776gmail.com>
2025-11-06 13:03:02 +0530

    tailwind-project

--- tailwindProject/razorpayClone2/tailwind.config.js
+++ tailwindProject/razorpayClone2/tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+// @type {import('tailwindcss').Config}
 module.exports = {
-  content: ["*"],
+  content: ["./tailwindProject/**/*.{html,js}"],
+
   theme: {
     extend: {
       fontFamily: {
@@ -17,7 +18,24 @@ module.exports = {
         grayBlue: "#344a6c",
         deepBlueHead: "#162f56",
         gray2: "#525a76",
-      },},
+      },
+      keyframes: {
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-up': 'fade-in-up 0.5s ease-out' 
+      }
+     
+    },
   },
   plugins: [],
 }
